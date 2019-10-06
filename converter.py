e2668e7e0664bca46ca2c57d192a52d4eb5e1a2a
Danny Lee <dannylhy@gmail.com>
2019-10-06 14:40:10 -0700

    Add command line parsing

--- converter.py
+++ converter.py
@@ -1,13 +1,27 @@
 # Simple converter
 
 import datetime
+import getopt
 import sys
 
 def int_timestamp_to_string(int_timestamp):
   dt_timestamp = datetime.fromtimestamp(int_timestamp)
   return dt_timestamp.strftime()
 
-def main():
-  print("Input Timestamp: ", sis.argv[0])
+def usage():
+  print ('converter.py -i <int_timestamp> -s <str_timestamp>')
+  
+def main(argv):
+  try:
+    opts, args = getopt.getopt(argv, "hi:s:", ["help","int_ts=","str_ts="])
+  except getopt.GetoptError:
+    usage()
+    sys.exit(2)
+  for o, a in opts:
+    if o == "-t":
+      int_timestamp_to_string(a)
+    elif o in ("-h", "--help"):
+      usage()
+      sys.exit(2)
 
-main()
+main(sys.argv[1:])
